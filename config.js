67136272273453a7ad54fce2e9414a78e6865362
4ndreid <ekoandrewicaksono@gmail.com>
2026-05-10 22:44:11 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Konfigurasi aplikasi frontend.
-// Ganti API_URL dengan URL deployment Apps Script terbaru milik Anda.
-// Token ini harus sama dengan API_TOKEN di file code.gs.
-window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbz0CqFTlxJHW8EazLMhp7mZRzzqbUNzSW_SV5ITCoHIIgxiLnlHQESShBRfILDunhib/exec",
-  API_TOKEN: "DashboardKedatangan2026"
-};
